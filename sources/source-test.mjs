9fb2ab0cbd2c57b7118f81a82b49da88e4598720
bernarth <alberthbernarth75@gmail.com>
2024-05-27 17:23:13 -0400

    add testing prop and console.log

--- sources/source-test.mjs
+++ sources/source-test.mjs
@@ -13,14 +13,20 @@ export default {
       description: 'The word to search for.',
       optional: false,
       default: 'hello'
-    }
+    },
+    testProp: {
+      type: 'string',
+      label: 'Test Prop',
+      description: 'The test prop.',
+      optional: false
+    },
   },
   methods: {},
   async run({ $ }) {
     const headers = {
       'Content-Type': 'application/json',
     };
-
+    console.log(this.word);
     const response = await axios.post(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`, {}, { headers });
 
     return response.data;
