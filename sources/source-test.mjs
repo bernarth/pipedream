0aab094d45a12df0f03724e9c21a4487d8b85bc3
bernarth <alberthbernarth75@gmail.com>
2024-05-27 17:13:34 -0400

    add new test source

---
+++ sources/source-test.mjs
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export default {
+  name: 'Source=Test',
+  version: '0.0.1',
+  key: 'source-test',
+  description: 'This is a public source test.',
+  type: 'source',
+  props: {
+    word: {
+      type: 'string',
+      label: 'Word',
+      description: 'The word to search for.',
+      optional: false,
+      default: 'hello'
+    }
+  },
+  methods: {},
+  async run({ $ }) {
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    const response = await axios.post(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`, {}, { headers });
+
+    return response.data;
+  },
+};
